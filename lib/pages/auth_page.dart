4ae1d5b31714789445d615970a223e59d8e0cda5
Adriano Gomes <kapiturasp@gmail.com>
2022-04-19 01:17:28 -0300

    Aula 301 -  Tela de Autenticação and example of cascade operator;

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -15,14 +17,43 @@ class AuthPage extends StatelessWidget {
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(255, 188, 117, 0.9),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  // using cascade operator ..
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        )
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Theme.of(context).accentTextTheme.headline6?.color,
+                    ),
+                  ),
+                ),
               ],
             ),
           )
@@ -31,3 +62,19 @@ class AuthPage extends StatelessWidget {
     );
   }
 }
+
+// Exemplo usado para explicar o cascade operator
+// void main() {
+//   List<int> a = [1, 2, 3];
+//   a.add(4);
+//   a.add(5);
+//   a.add(6);
+
+//   // cascade operator!
+//   a
+//     ..add(7)
+//     ..add(8)
+//     ..add(9);
+
+//   print(1);
+// }
