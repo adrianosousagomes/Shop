0343b0b018347b64e2a5b3901aa0f1c2b1dd0da4
Adriano Gomes <kapiturasp@gmail.com>
2022-04-19 23:25:25 -0300

    Aula 304 - Registrando Usuário; - Criando Provider Auth; - Cadastrando usuário;

---
+++ lib/models/auth.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  static const _url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC1Eaj5kb9UKBB_9pzsBiOIxjaRq-l7Puo';
+
+  Future<void> signup(String email, String password) async {
+    final response = await http.post(
+      Uri.parse(_url),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+
+    print(jsonDecode(response.body));
+  }
+}
