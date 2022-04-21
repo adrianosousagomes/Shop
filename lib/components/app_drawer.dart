ff7aedad16f48abb6e18bf0c7e9eabfaa804d345
Adriano Gomes <kapiturasp@gmail.com>
2022-04-20 22:08:42 -0300

    Aula 312 - Implementando o Logout;

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/utils/app_routes.dart';
+
+import '../models/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -18,7 +21,8 @@ class AppDrawer extends StatelessWidget {
             leading: const Icon(Icons.shop),
             title: const Text('Loja'),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed(AppRouters.AUTH_OR_HOME);
+              Navigator.of(context)
+                  .pushReplacementNamed(AppRouters.AUTH_OR_HOME);
             },
           ),
           const Divider(),
@@ -35,6 +39,17 @@ class AppDrawer extends StatelessWidget {
             title: const Text('Gerenciar Produtos'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRouters.PRODUCTS);
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              Provider.of<Auth>(context, listen: false).logout();
+              Navigator.of(context).pushReplacementNamed(
+                AppRouters.AUTH_OR_HOME,
+              );
             },
           )
         ],
